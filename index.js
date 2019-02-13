7f4b3f2cbd35d036372a1e2a61080a44987002b0
Daniel <d.solodov@wis.software>
2019-02-13 09:14:23 +0300

    Add cover-photo uploader

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-const app = require('express')();
+require('dotenv').config();
+
+const express = require('express');
+const easyvk = require('easyvk');
 const { interval: RxInterval$ } = require('rxjs');
+
+const app = express();
+app.use("/covers", express.static('covers'));
 
 //settings
 const { port } = require('./lib');
-require('dotenv').config();
-
 
 app.listen(port, serve);
 
-function serve() {
-  console.log(`server listen port ${port}`);
-  console.log(`${process.env.LOGIN}`);
+async function serve() {
+  try {
+    await updatePhoto();
+    console.log('photo was updated');
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
+}
 
-  // RxInterval$(1000)
-  //   .subscribe(()=>{
-  //     console.log(1);
-  //   });
+
+async function updatePhoto() {
+  const vk = await easyvk({
+    access_token: process.env.ACCESS_TOKEN,
+    save_session: true,
+  });
+
+  const fileData = await uploadPhoto(vk);
+
+  return vk.call('photos.saveOwnerCoverPhoto', fileData);
+}
+
+async function uploadPhoto({ uploader }) {
+  const uploadParams = {
+    group_id: process.env.GROUP_ID,
+    crop_x2: 1590,
+    crop_y2: 400,
+  }
+
+  const { url: uploadUrl } = await uploader.getUploadURL('photos.getOwnerCoverPhotoUploadServer', uploadParams);
+  const { vkr: fileData } = await uploader.uploadFile(uploadUrl, `${__dirname}/covers/test.jpeg`, 'photo');
+
+  return fileData;
 }
